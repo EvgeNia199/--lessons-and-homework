7dfa1982bbdd826be70b564e1d32576baf75d3cb
EvgeNia199 <evgeniakhoroshun@gmail.com>
2022-06-04 14:09:29 +0300

    Map, Obj

--- lessons/lesson13-js/index.js
+++ lessons/lesson13-js/index.js
@@ -68,9 +68,8 @@ function isBigEnough(element, index, array) {
     return element >= 18;
   }
 
-  [12, 5, 8, 130, 44].every(isBigEnough);   // false
-[12, 54, 18, 130, 44].every(isBigEnough); // true
-
+  [12, 5, 8, 130, 44].every(isBigEnough);  
+[12, 54, 18, 130, 44].every(isBigEnough); 
 // some
 
 function isSomeAdult1(arr) {
