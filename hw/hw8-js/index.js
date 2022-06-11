b83e272c20e94c3822911e0ced68ea278681ac05
EvgeNia199 <evgeniakhoroshun@gmail.com>
2022-06-11 14:00:25 +0300

    наследование классов,ошибки

--- hw/hw8-js/index.js
+++ hw/hw8-js/index.js
@@ -1,11 +1,11 @@
 // Task 1 Создайте объект ноутбука 
 
 const notebook = {
-    serialNumber: '3V5E7EA',
+    serialNumber: '     3V5E7EA     ',
     model: 'HP 255 G8',
     display: {
-    typeDisplay: 'IPS',
-    resolutionDisplay: ' 1920x1080'
+    typeDisplay: 'IPS ',
+    resolutionDisplay: '1920x1080 '
     },
     proccesor: 'Восьмиядерный AMD Ryzen 7 5700U (1.8 - 4.3 ГГц)',
     RAM: '8 ГБ',
@@ -16,11 +16,25 @@ const notebook = {
     frame: {
     color: 'silvery',
     weight: 1.7,
-    dimensions: ' (Ш х Г х В) 358 x 242 x 19'
+    dimensions: '  (Ш х Г х В) 358 x 242 x 19   '
     }
 }
-
+Object.entries(notebook).forEach(([key,value]) => {
+    if(typeof value === 'string'){
+        notebook[key] = value.trim()
+    } else if( (!!value) && (value.constructor === Object)){
+        Object.entries(value).forEach(([nestedKey,nestedValue])=> {
+            if(typeof nestedValue === 'string'){
+                console.log('nestedValue');
+                console.log(nestedValue);
+                value[nestedKey] = nestedValue.trim()
+            }
+        })
+    }
+})
+console.log('TASK 1');
 console.log(notebook);
+
 
 // Task 2 Создайте класс который описывает создание нового пользователя с помощью логина и пароля. Сделайте несколько обьектов. 
 // Создайте массив из нескольких обьектов (не вводите id)
@@ -59,7 +73,7 @@ const task2 = () => {
     return containerArr;
   
 }
-
+console.log('TASK 2');
 console.log(task2());
 
 
@@ -67,6 +81,7 @@ console.log(task2());
 
 const userMap = new Map();
 userMap.set(userTom.login, task2());
+console.log('TASK 3');
 console.log(userMap);
 
 // Task 4* разными способами решите вопрос соединения двух массивов без повторов
@@ -76,6 +91,7 @@ console.log(userMap);
 const fruit = ["Яблоко","Персик"];
 const vegetable = ["Огурец","Капуста"];
 const  wayFirst = [...fruit, ...vegetable]
+console.log('TASK 4.1');
 console.log(wayFirst);
 
 //4.2
@@ -87,9 +103,11 @@ for(let i = 0; i < array2.length; i++ ){
 return array1;
 }
 
+console.log('TASK 4.2');
 console.log(waySecond(fruit, vegetable));
 
 // 4.3
 
 const wayThird = fruit.concat(vegetable);
+console.log('TASK 4.3');
 console.log(wayThird);
