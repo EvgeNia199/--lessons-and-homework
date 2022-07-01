d09362376bde916939d442efa4fc923085d9061e
EvgeNia199 <evgeniakhoroshun@gmail.com>
2022-07-01 22:57:00 +0300

    todo

--- hw/hw11-todo/form1.js
+++ hw/hw11-todo/form1.js
@@ -33,71 +33,28 @@
 // }
 
 
-// const list = document.getElementById('tasks');
-// const btnAdd = document.getElementById('btnAdd');
-// const btnDel = document.getElementById('btnDel');
-// const task = document.getElementById('task');
-// // const deadline = document.getElementById('deadline')
-// // const deadlineList = document.getElementById('deadlineList')
-
-// btnAdd.addEventListener('click', addNewTask);
-// btnDel.addEventListener('click', deleteCheckTasks);
-// // btnAdd.addEventListener('click', taskDeadline);
-
-
-
-// function taskDeadline(event){
-//     const date = document.createElement('li');
-//     date.innerText = deadline.value;
-  
-//     deadlineList.append(date);
-    
-//     deadlzine.value = '';
-  
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function addNewTask(event) {
-//     const item = document.createElement('li');
-//     item.innerText = task.value;
-//     item.append(addCheckbox());
-//     list.append(item);
-//     task.value = "";
-// };
-
-// function addCheckbox(){
-//     const checkElem = document.createElement('input');
-//     checkElem.type = 'checkbox';
-//     checkElem.classList.add('check');
-//     return checkElem;
-    
-// }
-
-// function deleteCheckTasks(event){
-//     const deletedTasks = document.querySelectorAll('input[type="checkbox"]:checked');
-//     deletedTasks.forEach(el => el.parentNode.remove());
-// }
-
 const list = document.getElementById('tasks');
 const btnAdd = document.getElementById('btnAdd');
 const btnDel = document.getElementById('btnDel');
 const task = document.getElementById('task');
+const deadline = document.getElementById('deadline')
+const deadlineList = document.getElementById('deadlineList')
 
 btnAdd.addEventListener('click', addNewTask);
 btnDel.addEventListener('click', deleteCheckTasks);
+btnAdd.addEventListener('click', taskDeadline);
+
+
+
+function taskDeadline(event){
+    const date = document.createElement('li');
+    date.innerText = deadline.value;
+  
+    deadlineList.append(date);
+    
+    deadlzine.value = '';
+
+}
 
 function addNewTask(event) {
     const item = document.createElement('li');
@@ -112,9 +69,11 @@ function addCheckbox(){
     checkElem.type = 'checkbox';
     checkElem.classList.add('check');
     return checkElem;
+    
 }
 
 function deleteCheckTasks(event){
     const deletedTasks = document.querySelectorAll('input[type="checkbox"]:checked');
     deletedTasks.forEach(el => el.parentNode.remove());
 }
+
