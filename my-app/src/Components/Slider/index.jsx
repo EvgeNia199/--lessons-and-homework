f41e637a130a82b80ef4372bc0abc18a609ddc62
EvgeNia199 <evgeniakhoroshun@gmail.com>
2022-07-28 15:41:06 +0300

    Slider

--- my-app/src/Components/Slider/index.jsx
+++ my-app/src/Components/Slider/index.jsx
@@ -5,33 +5,33 @@ state: currentImgId 0
 setPrevSlide()
 setNextSlide()
 */
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import Controller from './Controller';
-import styles from './Slider.module.scss';
 
-export default function Slider(props) {
-    [currentImgId, setCurrentImgId] = useState(0);
-    const {images} = props;
-    const setPrevSlide = () => {
-        currentImgId == 0 ? 
-        setCurrentImgId(images.length - 1) : 
-        setCurrentImgId(currentImgId - 1);
-    }
-    const setNextSlide = () => {
-        currentImgId == images.length-1 ? 
-        setCurrentImgId(0) : 
-        setCurrentImgId(currentImgId + 1);
-    }
-    return(
-        <div className={styles.container}>
-            <Slide images={images} currentImgId={currentImgId}/>
-            <Controller setPrevSlide={setPrevSlide} setNextSlide={setNextSlide} />
-        </div>
-    )
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Controller from "./Controller";
+import styles from "./Slider.module.scss";
+import Slide from "./Slide";
+import { images } from "./SliderData";
+
+export default function Slider() {
+  const [currentImgId, setCurrentImgId] = useState(0);
+
+  const setPrevSlide = () => {
+    currentImgId == 0
+      ? setCurrentImgId(images.length - 1)
+      : setCurrentImgId(currentImgId - 1);
+  };
+  const setNextSlide = () => {
+    currentImgId == images.length - 1
+      ? setCurrentImgId(0)
+      : setCurrentImgId(currentImgId + 1);
+  };
+  return (
+    <div className={styles.mainContainer}>
+      <Slide images={images} currentImgId={currentImgId} />
+      <Controller setPrevSlide={setPrevSlide} setNextSlide={setNextSlide} />
+    </div>
+  );
 }
 
-Slider.propTypes = {
-    images: PropTypes.array.isRequired,
-};
-Footer
+
