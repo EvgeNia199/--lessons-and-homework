945519d47b0418cb4676bc8a8f88dbd58a1b95b4
EvgeNia199 <evgeniakhoroshun@gmail.com>
2022-10-23 14:21:03 +0300

    Feat add courses in freshcode

--- sql-db-learn/src/db/migrations/20221017144210-create-product.js
+++ sql-db-learn/src/db/migrations/20221017144210-create-product.js
@@ -40,7 +40,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Products');
